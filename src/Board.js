d9ed12e823b42c20b5415b7f25fc0cced176e024
Zac B <zacbennett0@gmail.com>
2019-01-06 20:29:23 -0800

    first commit

---
+++ src/Board.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Board.css';
+import Header from './Header';
+import Grid from './Grid';
+
+class Board extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerTurn: true,
+      board: [
+        [false, false, false],
+        [false, false, false],
+        [false, false, false]
+      ]
+    };
+  }
+  render() {
+    return (
+      <div className="Board">
+        {/* Contains score, and whose turn it is */}
+        <Header/>
+        <Grid squares={this.state.board}/>
+      </div>
+    );
+  }
+}
+
+export default Board;
